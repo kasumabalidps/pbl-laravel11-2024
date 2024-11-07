83b009b68dd75f0f1e63c71f2332ce3a3f1875a7
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-11-07 18:08:50 +0800

    added Dosen & Mahasiswa Models

---
+++ app/Models/DosenModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DosenModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'dosen';
+    protected $primaryKey = 'nip';
+    protected $fillable = ['nama_pengguna', 'nama_lengkap', 'email', 'password'];
+
+    protected $hidden = ['password'];
+
+    protected function ambilNamaTable()
+    {
+        return $this->table;
+    }
+}
+
